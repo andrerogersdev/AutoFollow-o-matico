dc368495f11359dedd46a82748d51e935dc15bf9
andrerogersdev <rogersdevandre4@gmail.com>
2024-04-30 14:29:54 -0400

    changes

--- seguir.py
+++ seguir.py
@@ -9,19 +9,18 @@ from selenium.webdriver.edge.service import Service as EdgeService
 
 try:
     livro = openpyxl.load_workbook('C:\\Users\\Allya\\OneDrive\\Documentos\\Seguidores\\Planilha Instagram.xlsx')
-    rodar = True
+    Iniciar = True
 except FileNotFoundError:
-    roda = True
+    ColetarDados = True
 else:
-    roda = False
+    ColetarDados = False
 
-if roda:
+if ColetarDados:
     sg.theme('Reddit')
     layout = [
 
-        [sg.Text('-Email: \t\t'), sg.Input(key='Email',size=(30,1))],
+        [sg.Text('-Email ou Nome: \t'), sg.Input(key='Email',size=(30,1))],
         [sg.Text('-Senha: \t\t'),sg.Input(key='senha',password_char='#',size=(30,1))],
-        [sg.Text('-Seguidores: \t'),sg.Input(key='Qtd',size=(30,1))],
         [sg.Button('Confirmar')]
 
     ]
@@ -33,17 +32,16 @@ if roda:
     while True:
         eventos,valores = janela.read()
         if eventos == sg.WINDOW_CLOSED:
-            rodar = False
+            Iniciar = False
             break
         if eventos == 'Confirmar':
-            rodar = True
+            Iniciar = True
             email = valores['Email']
             senha = valores['senha']
-            qtd_Segui = int(valores['Qtd'])
             book = openpyxl.Workbook()
             book.create_sheet('Instagram')
             InstaPage = book['Instagram']
-            InstaPage.append([email,senha,qtd_Segui])
+            InstaPage.append([email,senha])
             book.save('Planilha Instagram.xlsx')
             janela.close()
             break
@@ -51,99 +49,117 @@ if roda:
 sleep(10)
 
 while True:
-    if rodar:
+    if Iniciar:
         #Navegador Edge
-        Edge_profile = 'user-data-dir=C:\\Users\\Allya\\AppData\\Local\\MicrosoftEdge\\User\\Whats'
+        Edge_profile = 'user-data-dir=C:\\Users\\Allya\\AppData\\Local\\MicrosoftEdge\\User\\instabot'
         opt = webdriver.EdgeOptions()
         opt.add_argument(Edge_profile)
         opt.add_argument("--window-size=1366,768")
         opt.add_argument('--mute-audio')
-        opt.add_argument('--headless')
+        #opt.add_argument('--headless')
         driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()),options=opt)
 
         driver.get('https://www.instagram.com/')
 
-        def clicar(x,id,input=0):
+
+
+        def clicar(tipo,valor,valordovalor,escrever=0):
             while True:
-                if input != 0:
+                if escrever != 0:
                     try:
-                        a = driver.find_element(x,id)
+                        a = driver.find_element("xpath",f"//{tipo}[contains(@{valor},'{valordovalor}')]")
                         a.click()
-                        durmir(3)
-                        a.send_keys(input)
+                        dormir(3)
+                        a.send_keys(escrever)
                         a.send_keys(Keys.ENTER)
                     except:
-                        durmir(10)
+                        dormir(10)
                     else:
+                        print("Clicado com sucesso!")
                         break
                 else:
                     try:
-                        a = driver.find_element(x,id)
+                        a = driver.find_element("xpath",f"//{tipo}[contains(@{valor},'{valordovalor}')]")
                         a.click()
                     except:
-                        durmir(10)
+                        print("não encontrado, esperando 1 segundo.")
+                        sleep(1)
                     else:
+                        print("clicado com sucesso!")
                         break
-                break
-        def durmir(tempo):
+
+
+        def dormir(tempo):
             print(f"Esperando {tempo} segundos")
             for c in range(0,tempo):
                 print(c)
                 sleep(1)
             print('-Espera Finalizada-')
+
+
         print('Abrindo site')
-        durmir(15)
+        dormir(15)
         livro = openpyxl.load_workbook('C:\\Users\\Allya\\OneDrive\\Documentos\\Seguidores\\Planilha Instagram.xlsx')
         Dados = livro['Instagram']
         for rows in Dados.iter_rows(max_row=1):
             email = rows[0].value
             senha = rows[1].value
-            qtd_Segui = rows[2].value
 
         def logar(email,senha):
-            logEmail = driver.find_element('xpath','//input[contains(@name,"username")]')
-            durmir(5)
-            logEmail.click()
-            durmir(5)
-            logEmail.send_keys(email) 
-            durmir(5)
-            logSenha = driver.find_element('xpath','//input[contains(@name,"password")]')
-            durmir(5)
-            logSenha.click()
-            logSenha.send_keys(senha)
-            durmir(5)
-            logSenha.send_keys(Keys.ENTER)
-            print('logando')
-            durmir(20)
-        
+            try:
+                logado = driver.find_element("xpath","//img[contains(@crossorigin,'anonymous')]").click()   
+            except:
+                print("Ainda não logado")
+
+                clicar("input","name","username",escrever=email)
+                # Eu poderia apagar as proximas 5 linhas, pois a mesma se resume a linha acima, mas deixarei por orgulho e para que toda vez que eu ver essas linhas resumidas que eu me lembre da minha genialidade
+
+                #logEmail = driver.find_element('xpath','//input[contains(@name,"username")]')
+                #dormir(5)
+                #logEmail.click()
+                #dormir(5)
+                #logEmail.send_keys(email)    
+                print("Nome inserido com sucesso!") 
+                dormir(5)
+                clicar("input","name","password",escrever=senha)
+                #logSenha = driver.find_element('xpath','//input[contains(@name,"password")]')
+                #dormir(5)
+                #logSenha.click()
+                #logSenha.send_keys(senha)
+                #dormir(5)
+                #logSenha.send_keys(Keys.ENTER)
+                print("senha inserida com sucesso")
+                sleep(1)
+                print('logando')
+            else:
+                print("Já logado")
+            dormir(20)
+
 
         def Seguir():
             driver.get("https://www.instagram.com/explore/people/")
-            durmir(10)
-            t = 0
-            for rows in Dados.iter_rows(max_row=1):
-                 qtd_Segui = rows[2].value
-            while qtd_Segui > 0:
+            dormir(10)
+            while True:
                 seguir = driver.find_elements("xpath","//*[contains(text(), 'Seguir')]")
-                b = 0
                 try:
+                    b = 0
                     for c in range(0,50):
                         seguir[c].click()
                         b += 1
-                        if b == 25:
-                            print('+25 seguidores, dormindo 10 minutos')
-                            durmir(600)
+                        if b == 20:
+                            print('+20 pessoas seguidas, dormindo 10 minutos')
+                            dormir(600)
+                            driver.refresh()
+                            dormir(15)
                         elif b == 50:
-                            print('+50 seguidores, dormindo 1 hora')
-                            durmir(3600)
+                            reiniciar == True
+                            break
                         print("+1 seguido")
-                        durmir(5)
-                    qtd_Segui -= b
-                    
+                        dormir(5) 
                 except:
-                    err = driver.save_screenshot('erro.png')
-                    print('erro, dormindo 1 hora')
-                    durmir(3600)
+                    driver.save_screenshot('erro.png')
+                    print('erro, reiniciando a página e dormindo 1 hora')
+                    dormir(3600)
                     reiniciar = True
                 else:
                     reiniciar = False
@@ -151,16 +167,13 @@ while True:
                 if reiniciar:
                     print('reiniciando página')
                     driver.refresh()
-                    durmir(10)
-
+                    dormir(10)
 
         try:
             logar(email,senha)
         except:
             None
-        durmir(5)
         Seguir()
-        durmir(5)
         driver.quit()
         break
     else:
